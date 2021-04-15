26e599484a9c4760bd9133b5359d041f8809b301
Celine Martem <77649435+celinemartem@users.noreply.github.com>
2021-04-15 15:18:00 +0800

    Display from API with Note Content
    modifications to include note content

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,30 +1,20 @@
 import 'package:vsc_apitrial/models/api_response.dart';
+import 'package:vsc_apitrial/models/note.dart';
 import 'package:vsc_apitrial/models/note_for_listing.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'dart:core';
 
 class NotesService {
-  //static const API = 'http://api.notes.programmingaddict.com';
-  static const API = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app/notes';
+  static const API = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app';
   static const headers = {'apiKey': 'dad64144-f705-4616-be79-e43e8709dd2d'};
 //+ '/vsc_apitrial'
   Future<APIResponse<List<NoteForListing>>> getNotesList() {
-    return http.get(Uri.parse(API), headers: headers).then((data) {
+    return http.get(Uri.parse(API + '/notes'), headers: headers).then((data) {
       if (data.statusCode == 200) {
         final jsonData = json.decode(data.body);
         final notes = <NoteForListing>[];
         for (var item in jsonData) {
-          // <>
-          final note = NoteForListing(
-            noteID: item['noteID'],
-            noteTitle: item['noteTitle'],
-            createDateTime: DateTime.parse(item['createDateTime']),
-            latestEditDateTime: item['latestEditDateTime'] != null
-                ? DateTime.parse(item['latestEditDateTime'])
-                : null,
-          );
-          notes.add(note);
+          notes.add(NoteForListing.fromJson(item));
         }
         return APIResponse<List<NoteForListing>>(data: notes);
       }
@@ -32,5 +22,18 @@ class NotesService {
           error: true, errorMessage: 'An error occured');
     }).catchError((_) => APIResponse<List<NoteForListing>>(
         error: true, errorMessage: 'An error occured'));
+  }
+
+  Future<APIResponse<Note>> getNote(String noteID) {
+    return http
+        .get(Uri.parse(API + '/notes/' + noteID), headers: headers)
+        .then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        return APIResponse<Note>(data: Note.fromJson(jsonData));
+      }
+      return APIResponse<Note>(error: true, errorMessage: 'An error occured');
+    }).catchError((_) =>
+            APIResponse<Note>(error: true, errorMessage: 'An error occured'));
   }
 }
--- lib/views/note_modify.dart
+++ lib/views/note_modify.dart
@@ -1,40 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:vsc_apitrial/models/note.dart';
+import 'package:vsc_apitrial/services/notes_service.dart';
 
-class NoteModify extends StatelessWidget {
+class NoteModify extends StatefulWidget {
   final String noteID;
-  bool get isEditing => noteID != null;
-
   NoteModify({this.noteID});
+
+  @override
+  _NoteModifyState createState() => _NoteModifyState();
+}
+
+class _NoteModifyState extends State<NoteModify> {
+  bool get isEditing => widget.noteID != null;
+
+  NotesService get notesService => GetIt.I<NotesService>();
+
+  String errorMessage;
+  Note note;
+
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _isLoading = true;
+    });
+    notesService.getNote(widget.noteID).then((response) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (response.error) {
+        errorMessage = response.errorMessage ?? 'An error occurred';
+      }
+      note = response.data;
+      _titleController.text = note.noteTitle;
+      _contentController.text = note.noteContent;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(isEditing ? 'Edit note' : 'Create Note')),
+      appBar: AppBar(title: Text(isEditing ? 'Edit note' : 'Create note')),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(hintText: 'Title'),
-            ),
-            Container(height: 8),
-            TextField(
-              decoration: InputDecoration(hintText: 'Content'),
-            ),
-            Container(height: 16),
-            SizedBox(
-              width: double.infinity,
-              height: 35,
-              child: RaisedButton(
-                child: Text('Submit', style: TextStyle(color: Colors.white)),
-                color: Theme.of(context).primaryColor,
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                children: <Widget>[
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(hintText: 'Note title'),
+                  ),
+                  Container(height: 8),
+                  TextField(
+                    controller: _contentController,
+                    decoration: InputDecoration(hintText: 'Note content'),
+                  ),
+                  Container(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 35,
+                    child: RaisedButton(
+                      child:
+                          Text('Submit', style: TextStyle(color: Colors.white)),
+                      color: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
